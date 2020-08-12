a934447437877a49cbcadba19232f01ce0ce2628
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-08-12 16:28:12 +0900

    reservationモデル、コントローラーの作成

---
+++ db/migrate/20200812065743_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reservations do |t|
+      t.datetime :date, null:false
+      t.integer :reason, null:false
+      t.string :other_reason, limit: 255
+      t.integer :status, null:false, default: 0
+
+      t.timestamps
+    end
+  end
+end
