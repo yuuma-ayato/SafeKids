4ca5e6389c61b34119b93ad970b686f02e36fb5e
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-08-14 19:41:56 +0900

    other_reasonカラムにバリデーションを定義

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,14 @@
 class Reservation < ApplicationRecord
-    enum reason: { 仕事: 0, 疾病: 1, その他: 2 }
+  before_validation do
+    self.other_reason = nil unless reason == "その他"
+  end
+
+  validates :other_reason, length: { maximum: 255 }, presence: true, if: :otherreason?
+
+  def otherreason?
+    reason == "その他"
+  end
+
+    enum reason: { その他: 0, 仕事: 1, 疾病: 2 }
     enum status: { 仮予約: 0, 本予約: 1 }
 end
