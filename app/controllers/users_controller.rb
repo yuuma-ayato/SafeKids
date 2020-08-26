3d57c36eeea70f0cf495b066df0dfe19a2529d27
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-08-26 17:22:40 +0900

    エラーメッセージ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :check_contact
+  # before_action :check_child
+
   def show
     @user = User.find(params[:id])
     @contact = Contact.find(params[:id])
@@ -6,3 +10,16 @@ class UsersController < ApplicationController
     @name = Name.find(params[:id])
   end
 end
+
+# private
+# def check_contact
+#   if @contact.nil?
+#     redirect_to new_contact_path, notice: "連絡先が未入力です。"
+#   end
+# end
+#
+# def check_child
+#   if @child.nil?
+#     redirect_to new_child_path, notice: "お子様情報が未入力です。"
+#   end
+# end
