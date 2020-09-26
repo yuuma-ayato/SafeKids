3d2d1846911c85d0031940795a3b11bf07eb3fc5
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-09-26 17:44:18 +0900

    search_contactsメソッドをreservations_helperに定義

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -4,4 +4,18 @@ module ReservationsHelper
         .where(id: current_user.id)
         .select("users.*, children.image, names.family_name, names.first_name")
   end
+
+  def search_contacts
+    User.joins(contacts: :names)
+        .where(id: @reservation.user_id)
+        .select(
+          "users.*,
+          names.family_name,
+          names.first_name,
+          names.family_name_kana,
+          names.first_name_kana,
+          contacts.phone_number,
+          contacts.relation,
+          contacts.other_relation")
+  end
 end
