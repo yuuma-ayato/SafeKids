bafad3607b7c1d6bf70fc0f7b86f040edbeb9aec
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-10-09 13:59:26 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,10 @@
 FactoryBot.define do
   factory :user do
-    id { 1 }
     email { 'sample@example.com' }
     password { '00000000' }
     admin { false }
   end
   factory :admin_user, class: User do
-    id { 2 }
     email { 'admin@example.com' }
     password { '00000000' }
     admin { true }
