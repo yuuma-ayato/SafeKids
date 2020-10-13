27325142c17608647321afb2f9cbb53dabecfd55
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-10-13 19:02:02 +0900

    コントローラーのif文をcase文に修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,7 +5,8 @@ module Users
     before_action :check_guest, only: :create
 
     def check_guest
-      if params[:user][:email].downcase == 'guest@example.com' || 'guest_clerk@example.com' || 'guest_childminder@example.com'
+      case params[:user][:email].downcase
+      when ['guest@example.com', 'guest_clerk@example.com', 'guest_childminder@example.com']
         redirect_to edit_user_registration_path alert: 'ゲストユーザーの変更・削除はできません。'
       end
     end
